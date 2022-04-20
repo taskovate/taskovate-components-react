a9d6737996f47d0a50b687521021745d9896a6bf
Elijah Montenegro <ejmonte@outlook.com>
2022-04-20 18:48:24 -0500

    added module aliases for theme

--- src/components/UIShell/Header.tsx
+++ src/components/UIShell/Header.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled, { useTheme } from 'styled-components';
-// import { colors, gridSize, borderRadius, gradients }
+import { gridSize, layers, animation, borderRadius, gradients } from '@theme/constants';
+
 // type User = {
 //   name: string;
 // };
@@ -11,46 +12,39 @@ import styled, { useTheme } from 'styled-components';
 //   offset: string;
 // }
 
-const Container = styled.div`
-  ${({ theme }) => `
-    z-index: ${theme.layers.navigation()};
+const HeaderWrapper = styled.div<any>`
+  z-index: ${layers.navigation()};
+  transition: ${animation.normal()};
 
-    ::after {
-      display: flex;
-      content: '';
-      position: sticky;
-      height: ${theme.gridSize() * 7}px;
-      background-color: ${theme.colors.red[500]};
-      margin-top: ${theme.gridSize() * 1}px;
-      top: ${theme.gridSize() * 1}px;
-      width: calc(100% - ${theme.gridSize() * 2.5}px);
-      border-radius: ${theme.borderRadius() * 0.75}px;
-    }
+  ::after {
+    display: flex;
+    content: '';
+    position: sticky;
+    height: ${gridSize() * 7}px;
+    background-color: ${({ theme }) => theme.background()};
+    margin-top: ${gridSize() * 1}px;
+    top: ${gridSize() * 1}px;
+    width: calc(100% - ${gridSize() * 6.5}px);
+    border-top-right-radius: ${borderRadius() * 0.6}px;
+    border-bottom-right-radius: ${borderRadius() * 0.6}px;
+    padding: 0 ${gridSize() * 2}px;
+  }
 
-    ::before {
-      display: flex;
-      content: '';
-      position: absolute;
-      background: ${theme.gradients.primary()};
-      width: calc(100% - ${theme.gridSize() * 2}px);
-      height: ${theme.gridSize() * 8}px;
-      margin-top: -${theme.gridSize() * 0.5}px;
-      border-radius: ${theme.borderRadius() * 1}px;
-    }
-  `}
+  ::before {
+    display: flex;
+    content: '';
+    position: absolute;
+    background: ${gradients.primary()};
+    width: calc(100% - ${gridSize() * 2}px - ${gridSize() / 4}px);
+    height: calc(${gridSize() * 7}px + ${gridSize() / 2}px);
+    top: calc(${gridSize() * 1}px - ${gridSize() / 8}px);
+    margin-top: -${gridSize() / 8}px;
+    border-top-right-radius: ${borderRadius() * 1}px;
+    border-bottom-right-radius: ${borderRadius() * 1}px;
+    opacity: ${({ mode }) => mode === 'scroll' ? 0 : 1};
+  }
 `;
 
-const Border = styled.div`
-  ${({ theme }) => `
-      display: block;
-      position: fixed;
-      content: '';
-      background-color: green;
-      width: 100%;
-      height: ${theme.gridSize() * 8}px;
-      z-index: 0;
-  `}
-`;
 
 const Header: React.FC<any> = (props: any) => {
   const theme = useTheme();
@@ -70,7 +64,7 @@ const Header: React.FC<any> = (props: any) => {
   
   return (
     <>
-      <Container />
+      <HeaderWrapper mode={mode}/>
       {/* <Border /> */}
     </>
   );
