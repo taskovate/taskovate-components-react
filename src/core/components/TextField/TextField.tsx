11125d823c632bd9b9696dfa7339ca3e7623c1e3
Elijah Montenegro <ejmonte@outlook.com>
2022-04-30 02:22:55 -0500

    added select component

--- src/core/components/TextField/TextField.tsx
+++ src/core/components/TextField/TextField.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, cloneElement, useState  } from 'react';
 import styled from 'styled-components';
 import { colors, gridSize, layers, animation, fontSize, borderRadius, gradients, fontSizeSmall } from '@theme/constants';
+import { inputStyles } from 'core/theme/dark';
 
 const Styled = styled.div`
   display: flex;
@@ -8,27 +9,34 @@ const Styled = styled.div`
 
 const Input = styled.input`
   display: flex;
+  transition: ${animation.normal()};
   padding: ${gridSize() * 0.75}px ${gridSize() * 1.25}px;
   border-radius: ${borderRadius() * 1}px;
-  border: ${gridSize() * 0.25}px solid transparent;
-  background-color: ${colors.d[600]};
-  color: ${({ theme }) => theme.text()};
+  border: ${gridSize() * 0.25}px solid ${({ theme: { inputStyles } }) => inputStyles.borderColor['body'].default()};;
+  background-color: ${({ theme: { inputStyles } }) => inputStyles.background['body'].default()};
+  color: ${({ theme: { inputStyles } }) => inputStyles.color['body'].default()};
   outline: none;
+  min-width: 0;
   :hover {
-    border-color: ${colors.p[500]};
+    border-color: ${({ theme: { inputStyles } }) => inputStyles.borderColor['body'].hover()};
   }
   :focus {
-    border-color: ${colors.p[400]};
+    border-color: ${({ theme: { inputStyles } }) => inputStyles.borderColor['body'].focus()};
   }
+  ::placeholder {
+    color: ${({ theme: { inputStyles } }) => inputStyles.color['body'].placeholder()};
+  }
+  min-width: 200px;
 `;
 
 const TextField = ({
+  placeholder,
   children
 }: any) => {
 
   return (
     <Styled>
-      <Input />
+      <Input placeholder={placeholder} />
     </Styled>
   );
 };
