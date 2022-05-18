1818ee94d942093ff7fa6aec6665791f9fe419bc
Elijah Montenegro <ejmonte@outlook.com>
2022-05-17 22:33:18 -0500

    update

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -2,18 +2,15 @@ import { makeVar } from '@apollo/client';
 
 export const Space = {
   spaceItemsVar: makeVar([
-    { value: "atx_qa", label: "ATX QA", image: 'https://picsum.photos/96/96' },
-    { value: "uk_qa", label: "UK QA", image: 'https://picsum.photos/96/96' },
-    { value: "la_qa", label: "LA QA", image: 'https://picsum.photos/96/96' },
-    { value: "de_qa", label: "DE QA", image: 'https://picsum.photos/96/96' },
-    { value: "atx", label: "ATX", image: 'https://picsum.photos/96/96' },
-    { value: "uk", label: "UK", image: 'https://picsum.photos/96/96' },
-    { value: "la", label: "LA", image: 'https://picsum.photos/96/96' },
-    { value: "de", label: "DE", image: 'https://picsum.photos/96/96' },
-    { value: "qa_atx", label: "ATX E", image: 'https://picsum.photos/96/96' },
-    { value: "qa_uk", label: "UK E", image: 'https://picsum.photos/96/96' },
-    { value: "qa_la", label: "LA E", image: 'https://picsum.photos/96/96' },
-    { value: "qa_de", label: "DE E", image: 'https://picsum.photos/96/96' },
+    { value: "1", label: "Chocolate", image: 'https://picsum.photos/96/96' },
+    { value: "2", label: "Strawberry", image: 'https://picsum.photos/96/96' },
+    { value: "3", label: "Vanilla", image: 'https://picsum.photos/96/96' },
+    { value: "4", label: "Coffee", image: 'https://picsum.photos/96/96' },
+    { value: "5", label: "Mustard", image: 'https://picsum.photos/96/96' },
+    { value: "6", label: "Taco", image: 'https://picsum.photos/96/96' },
+    { value: "7", label: "Tea", image: 'https://picsum.photos/96/96' },
+    { value: "8", label: "Soda", image: 'https://picsum.photos/96/96' },
+    { value: "9", label: "Popcorn", image: 'https://picsum.photos/96/96' }
   ]),
   starredItemsVar: makeVar([]),
 };
