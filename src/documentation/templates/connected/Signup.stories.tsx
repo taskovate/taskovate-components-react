6d6e41909c72654b1ece6a180c0e6b60f3c915ac
elijahmontenegro <ejmonte@outlook.com>
2022-06-08 18:15:35 -0500

    building signup template

--- src/documentation/templates/connected/Signup.stories.tsx
+++ src/documentation/templates/connected/Signup.stories.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { gridSize } from '@theme/constants';
+import { colors, gridSize } from '@theme/constants';
+import { MdLogin } from 'react-icons/md';
+import { HiOutlineChatAlt, HiOutlineBell, HiOutlineUser, HiHome, HiStar } from 'react-icons/hi';
+import {
+  Header,
+  HeaderDesignation,
+  HeaderMenuBar,
+  HeaderMenuItem,
+  HeaderGlobalBar,
+  HeaderGlobalAction,
+  Plate,
+  Button
+} from '@components/core';
+
+import {
+  PageLayout,
+  TopNavigation,
+  Content,
+  Footer,
+  Main,
+  LeftSidebar,
+  RightSidebar
+} from '@components/core';
 
 export default {
   title: 'Templates/Signup',
@@ -9,8 +31,68 @@ export default {
     layout: 'fullscreen',
   },
 } as ComponentMeta<any>;
-
-export const Default: ComponentStory<any> = () => (
-  <>
-  </>
+// unique header needed (in main)
+//unique footer needed
+export const Default: ComponentStory<typeof Header> = () => (
+  <PageLayout>
+    <TopNavigation>
+      <Header>
+        <HeaderDesignation />
+        <HeaderMenuBar>
+          <HeaderMenuItem>
+            About Taskovate
+          </HeaderMenuItem>
+          <HeaderMenuItem>
+            Solutions
+          </HeaderMenuItem>
+          <HeaderMenuItem>
+            Resources
+          </HeaderMenuItem>
+          <HeaderMenuItem>
+            Pricing
+          </HeaderMenuItem>
+        </HeaderMenuBar>
+        <HeaderGlobalBar>
+          <HeaderGlobalAction>
+            Log in
+          </HeaderGlobalAction>
+          <HeaderGlobalAction appearance="primary">
+            Try Taskovate for Free
+          </HeaderGlobalAction>
+        </HeaderGlobalBar>
+      </Header>
+    </TopNavigation>
+    <Content>
+      <Main>
+      </Main>
+    </Content>
+    <Footer>
+      <div>
+      </div>
+      <div>
+        <div>
+          <h4>Resources</h4>
+          <p>Support</p>
+          <p>Blog</p>
+          <p>Newsletter</p>
+          <p>What's new</p>
+          <p>Sitemap</p>
+        </div>
+        <div>
+          <h4>Company</h4>
+          <p>Your Privacy</p>
+          <p>About us</p>
+        </div>
+        <div>
+          <h4>Support</h4>
+          <p>Contact Us</p>
+          <p>FAQ</p>
+        </div>
+      </div>
+      <div>
+        <h2>Taskovate<span style={{ fontSize: 18 }}>.com</span></h2>
+        <p>All rights reserved. © 2022 Taskovate.com</p>
+      </div>
+    </Footer>
+  </PageLayout>
 );
